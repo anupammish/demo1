8c233ca78bb0d914ba7e2affd1f7c158df70a5c3
anupammish <mishra.anupam941@gmail.com>
2022-08-26 19:13:29 +0530

    First Commit: This is first commit

---
+++ GitDemo2/src/main/java/AllModules/GitDemo2/App.java
@@ -0,0 +1,13 @@
+package AllModules.GitDemo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
